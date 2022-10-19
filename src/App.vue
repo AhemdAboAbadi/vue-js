1fb9cf4a5243eb24696b02d706d112a29a20310f
AhmedAboAbadi <ahmed.m.aboabadi@gmail.com>
2022-10-19 17:10:24 +0300

    init vue project and adding some style and create new components

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handelClick">click me</button>
+  <p> Welcome ...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" />
+  </div>
+  <button @click="toggleModal">open modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "Hello World :)",
+      header: "Sign up for the Giveway",
+      text: "hello from GSG",
+      showModal: false,
+    };
+  },
+  methods: {
+    handelClick() {
+      console.log(this.$refs.name.value);
+      this.$refs.name.classList.add("red");
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  font-size: 40px;
+  border-bottom: 1px solid #eee;
+  padding-bottom: 10px;
+}
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="backdrop">
+    <div class="modal" :class="{sale :  theme === 'sale'}">
+      <p>{{ header }}</p>
+      <h1>{{ text }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+};
+</script>
+
+<style scoped>
+.backdrop {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.modal {
+  position: fixed;
+  width: 400px;
+  border-radius: 10px;
+  background: white;
+  padding: 20px;
+  margin: 100px auto;
+}
+.modal h1 {
+  color: red;
+}
+</style>
